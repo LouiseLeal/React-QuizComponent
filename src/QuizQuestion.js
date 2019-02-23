f0480e6245a5525cd928f413396d9906b7ca26b7
Louise leal <lcloudine@gmail.com>
2019-02-23 18:31:43 -0400

    Creating a QuizQuestion Component_continue

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,16 +1,21 @@
 import React, {Component} from 'react'
 
+
+//props => quiz_qqiestion = {instruction_text: string,
+//                            answer_options: []
+//                             answer: string}
+
 class QuizQuestion extends Component{
 
     render(){
         return (
             <main>
                 <section>
-                <p>{this.props.instruction_tex}t</p>
+                <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                 <ul>
-                    //quiz question button logic goes here
+                    <li>{this.props.quiz_question.answer_options[0]}</li>
                 </ul>
                 </section>
             </main>
