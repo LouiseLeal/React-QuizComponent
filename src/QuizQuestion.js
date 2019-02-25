e3f4e923317a378125aafb2a87d5f557b95d448a
Louise leal <lcloudine@gmail.com>
2019-02-25 16:56:50 -0400

    fix failed test

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -11,7 +11,7 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 class QuizQuestion extends Component{
 
     handleClick(buttonText){
-        if(buttonText === this.props.answer){
+        if(buttonText === this.props.quiz_question.answer){
             this.props.showNextQuestionHandler();
         }
     }
