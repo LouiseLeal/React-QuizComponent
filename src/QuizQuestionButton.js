4a2c407bcc11919d5393f99d339e940e35e0deab
Louise leal <lcloudine@gmail.com>
2019-02-25 16:55:16 -0400

    Displaying Quiz Questions and Handling Events - 1 teste failed

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react'
 
 
 //prop=> button_text
+ //       key
+ //      clickHandler()
 
 class QuizQuestionButton extends Component{
 
+    handleClick(){
+        this.props.clickHandler(this.props.button_text)
+    }
+
     render(){
         return( 
-        <li>
-            <button>
-                {this.props.button_text}
-            </button>
+        <li key={this.props.key}>
+            <button value={this.props.button_text} onClick={this.handleClick.bind(this)}>{this.props.button_text} </button>
         </li>
         )
     }
